72e069d451ef29df16bee863046c147ab1583d46
Daniel Kouba <Koubaphotog@gmail.com>
2015-02-02 10:07:22 +0000

    amazon s3 support added

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'unicorn'
 gem 'bootstrap-sass', '~> 3.3.3'
 gem 'devise', '~> 3.4.1'
 gem 'paperclip', '~> 4.2.1'
+gem 'aws-sdk', '~> 1.61.0'
 
 group :development, :test do
     gem 'sqlite3', '~> 1.3.10'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,14 @@ Rails.application.configure do
   
   #added as per Devise install instructions
   config.action_mailer.default_url_options = { host: 'pinterestedclone.herokuapp.com' }
+ 
+ #amazon s3 
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
